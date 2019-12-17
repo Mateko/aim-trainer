3f3cbfd3609c625435a01a21386803948d5f6286
Mateko <mateuszpierzchala01@gmail.com>
2019-12-17 19:24:26 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Game from "./components/Game";
+import Home from "./components/Home";
+import Result from "./components/Result";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/game" component={Game} />
+        <Route path="/result" component={Result} />
+        />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
