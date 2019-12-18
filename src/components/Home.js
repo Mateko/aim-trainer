051c6339afe3b259c7efc84ae63475e547a6ceb1
Mateko <mateuszpierzchala01@gmail.com>
2019-12-18 18:56:17 +0100

    Completed menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,105 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.scss";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+class Home extends React.Component {
+  state = {
+    gameDuration: null,
+    gameDifficult: null
+  };
+
+  setDifficult(e) {
+    this.setState({
+      gameDifficult: e.target.value
+    });
+  }
+
+  setTime(e) {
+    this.setState({
+      gameDuration: e.target.value
+    });
+  }
+
+  render() {
+    const { gameDuration, gameDifficult } = this.state;
+
+    return (
+      <main className="page">
+        <header className="header">
+          <div className="header__logo">
+            <i className="material-icons header__icon">control_camera</i>
+            <p className="header__text">Aim trainer</p>
+          </div>
+        </header>
+        <section className="settings">
+          <h4 className="settings__header">
+            Time: {gameDuration ? gameDuration + " seconds" : null}
+          </h4>
+          <div className="settings__container">
+            <button
+              type="button"
+              className="settings__time"
+              value={20}
+              onClick={this.setTime.bind(this)}
+            >
+              10s
+            </button>
+            <button
+              className="settings__time"
+              type="button"
+              value={30}
+              onClick={this.setTime.bind(this)}
+            >
+              30s
+            </button>
+            <button
+              className="settings__time"
+              type="button"
+              value={60}
+              onClick={this.setTime.bind(this)}
+            >
+              60s
+            </button>
+          </div>
+          <h4 className="settings__header">
+            Level of difficulty: {gameDifficult}
+          </h4>
+          <div className="settings__container">
+            <button
+              className="settings__difficult"
+              type="button"
+              value="Easy"
+              onClick={this.setDifficult.bind(this)}
+            >
+              Easy
+            </button>
+            <button
+              className="settings__difficult"
+              type="button"
+              value="Medium"
+              onClick={this.setDifficult.bind(this)}
+            >
+              Medium
+            </button>
+            <button
+              className="settings__difficult"
+              type="button"
+              value="Hard"
+              onClick={this.setDifficult.bind(this)}
+            >
+              Hard
+            </button>
+          </div>
+          <span className="settings__border"></span>
+          <div className="settings__container">
+            <Link className="settings__buton" to="/game">
+              Start game
+            </Link>
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
 
 export default Home;
